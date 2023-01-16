344de593b7b488dda50b5a4b8e66e7f1fb1ac663
Juan Pablo Rivera Ruiz <a01636350@tec.mx>
2023-01-16 06:29:24 -0600

    Project structure

---
+++ stock_trading_sim.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+class StockTradingSimEnvironment:
+    def __init__(self, data, initial_investment):
+        pass
+
+    def reset(self):
+        # Resets all the variables and returns the initial state
+        pass
+
+    def step(self, action):
+        # Performs the given action in the environment
+        # Return the next state, the reward, whether we are in a terminal state and info dictionary
+        pass
